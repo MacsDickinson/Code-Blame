59515bf8569eb989da91116ac56b36b488af3be8
Macs Dickinson <macs.dickinson@hotmail.co.uk>
2013-10-30 21:06:47 +0000

    added blame class

---
+++ src/CodeBlame/Models/Blame.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeBlame.Models
+{
+    public class Blame
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Code { get; set; }
+        public DateTime DateAdded { get; set; }
+    }
+}
