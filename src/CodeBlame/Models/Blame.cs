2a756ecfef2560e090c6926efaa649063eab9164
Macs Dickinson <macs.dickinson@hotmail.co.uk>
2013-11-03 01:18:12 +0000

    Added title

--- src/CodeBlame/Models/Blame.cs
+++ src/CodeBlame/Models/Blame.cs
@@ -6,6 +6,7 @@ namespace CodeBlame.Models
     public class Blame
     {
         public int Id { get; set; }
+        public string Title { get; set; }
         public string Description { get; set; }
         public BlameLanguage Language { get; set; }
         public string Code { get; set; }
